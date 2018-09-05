473910cddafebb089114ead59c7f6d50f3080b4f
jdnidle <42973612+jdnidle@users.noreply.github.com>
2018-09-04 22:06:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ function respond() {
       botRegexSiege = /^\/siege/; botRegexCoaches = /^\/coaches/;
       botRegexBurn = /^\/burn/; botRegexWamb = /^\/wambulance/;
       botRegexMad = /^\/madbro/; botRegexPats = /^\/patslose/;
-      botRegexCheesy = /^\/austin/;
+      botRegexCheesy = /^\/austin/; botRegexWooo = /^\/wooo/;
       botRegexDaf = /^\/dafuq/; botRegexFOS = /^\/factoryofsadness/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -56,6 +56,11 @@ function respond() {
   else if(request.text && botRegexCheesy.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://media.giphy.com/media/oGBw7OaVBei1W/giphy.gif");
+    this.res.end();
+    }
+  else if(request.text && botRegexWooo.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://i.imgur.com/42haLq8.gif?noredirect");
     this.res.end();
   }  
   else if(request.text && botRegexCoaches.test(request.text)) {
